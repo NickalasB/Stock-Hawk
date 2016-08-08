b606dc6b2952d84518b7eb6b56ae6349b90c635f
Nick Bradshaw <nickalasb@gmail.com>
2016-08-08 16:48:19 -0700

    Finally managed to make a check for valid stock or not before adding it to database and learned how to display toast from a service

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -3,16 +3,24 @@ package com.sam_chordas.android.stockhawk.service;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+
 import com.google.android.gms.gcm.TaskParams;
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.rest.Utils;
 
 /**
  * Created by sam_chordas on 10/1/15.
  */
 public class StockIntentService extends IntentService {
+   Handler mHandler;
+
 
   public StockIntentService(){
     super(StockIntentService.class.getName());
+    mHandler = new Handler();
+
   }
 
   public StockIntentService(String name) {
@@ -29,5 +37,12 @@ public class StockIntentService extends IntentService {
     // We can call OnRunTask from the intent service to force it to run immediately instead of
     // scheduling a task.
     stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+    //here's where we check if the stock was valid from Utils
+    if (Utils.NO_SYMBOL){
+      Log.v("StockIntentService", "No symbol matches that input!!!");
+      mHandler.post(new DisplayToast(this, getString(R.string.invalid_stock_toast)));
+
+    }
   }
 }
+

a7a620d7265845683599b4f8fc68395dd1e13469
Nick Bradshaw <nickalasb@gmail.com>
2016-08-22 16:28:54 -0700

    Finished up RTL portion and fixed a few strings

--- app/src/main/java/com/sam_chordas/android/stockhawk/data/mappers/StockHistoryMapper.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/data/mappers/StockHistoryMapper.java
@@ -1,8 +1,6 @@
 package com.sam_chordas.android.stockhawk.data.mappers;
 
 import android.annotation.TargetApi;
-import android.content.Context;
-import android.content.res.Configuration;
 import android.os.Build;
 
 import com.github.mikephil.charting.data.Entry;
@@ -21,15 +19,6 @@ public class StockHistoryMapper {
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     private String LOG_TAG = StockHistoryMapper.class.getSimpleName();
-
-    private Context mContext;
-    private Configuration config;
-
-
-//    public StockHistoryMapper(){
-//        this.mContext = context;
-//
-//    }
 
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -57,45 +46,3 @@ public class StockHistoryMapper {
 
     }
 }
-
-
-//        config = mContext.getResources().getConfiguration();
-//
-//        if (config != null) {
-//            if (config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
-//                for (int i = 0; i < stockHistoryList.size(); i++) {
-//                    StockHistory stockHistory = stockHistoryList.get(i);
-//                    float stockHistoryClosePrice = Float.parseFloat(stockHistory.getClose());
-//                    entries.add(new Entry(stockHistoryClosePrice, chartIndex));
-//                    chartIndex++;
-//                }
-//                return entries;
-//            }
-//        }
-//    else {
-//            for (int i = stockHistoryList.size() - 1; i >= 0; i--) {
-//                StockHistory stockHistory = stockHistoryList.get(i);
-//                float stockHistoryClosePrice = Float.parseFloat(stockHistory.getClose());
-//                entries.add(new Entry(stockHistoryClosePrice, chartIndex));
-//                chartIndex++;
-//            }
-//            return entries;
-//    }
-//    return entries;
-//    }
-//
-//
-//
-//
-//    public Calendar convertStringToDate(String dateString) {
-//        Calendar calendar = Calendar.getInstance();
-//        try {
-//            calendar.setTime(dateFormat.parse(dateString));
-//        } catch (ParseException e) {
-//        }
-//
-//        return calendar;
-//
-//    }
-//}
-

53adb80908e72ea352cd5637d38b926aeaf4b634
Nick Bradshaw <nickalasb@gmail.com>
2016-08-25 03:30:07 -0700

    Trying to start from fresh with the widget. This is a good commit for the layout and simple intent to open the app on-click

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetIntentService.java
@@ -3,5 +3,5 @@ package com.sam_chordas.android.stockhawk.widget;
 /**
  * Created by nickbradshaw on 8/22/16.
  */
-public class SingleStockWidgetIntentService {
+public class StockWidgetIntentService {
 }

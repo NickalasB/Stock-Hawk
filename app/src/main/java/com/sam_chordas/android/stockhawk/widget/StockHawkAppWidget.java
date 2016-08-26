3cb8d0292b2312dbada6a942f15969d2a4d6d035
Nick Bradshaw <nickalasb@gmail.com>
2016-08-25 21:07:45 -0700

    WORKING widget app bar now opens the main activity. getting closer. Also deleted the duplicate widget info as this one is better

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockHawkAppWidget.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockHawkAppWidget.java
@@ -4,68 +4,73 @@ import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
 import android.widget.RemoteViews;
 
 import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.service.StockTaskService;
 import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+import com.sam_chordas.android.stockhawk.ui.MyStocksChartActivity;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class StockHawkAppWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(),
-                R.layout.stock_hawk_app_widget);
-
-        // Create an Intent to launch MainActivity
-        Intent launchIntent = new Intent(context, MyStocksActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
-
-        views.setOnClickPendingIntent(R.id.widget_title_bar, pendingIntent);
-
-
-        // Set up the collection
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            setRemoteAdapter(context, views);
-        } else {
-            setRemoteAdapterV11(context, views);
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (StockTaskService.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
         }
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-
     }
-
-
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            int layoutId = R.layout.stock_hawk_app_widget;
+
+            // Get the layout for the App Widget and attach an on-click listener
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_hawk_app_widget);
+
 
             // Create an Intent to launch MainActivity
             Intent launchIntent = new Intent(context, MyStocksActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.stock_widget_frame_layout, pendingIntent);
 
-            // Get the layout for the App Widget and attach an on-click listener
-            RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
-            views.setOnClickPendingIntent(R.id.widget_title_bar, pendingIntent);
+            // Set up the collection
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, views);
+            } else {
+                setRemoteAdapterV11(context, views);
+            }
+
+            Intent clickIntentTemplate = new Intent(context, MyStocksChartActivity.class);
+            PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.widget_list, clickPendingIntentTemplate);
+            views.setEmptyView(R.id.widget_list, R.id.widget_empty);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
-
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
 
 
     @Override
@@ -101,3 +106,28 @@ public class StockHawkAppWidget extends AppWidgetProvider {
     }
 }
 
+//    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+//                                int appWidgetId) {
+//
+//        // Construct the RemoteViews object
+//        RemoteViews views = new RemoteViews(context.getPackageName(),
+//                R.layout.stock_hawk_app_widget);
+//
+//        // Create an Intent to launch MainActivity
+//        Intent launchIntent = new Intent(context, MyStocksActivity.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
+//
+//        views.setOnClickPendingIntent(R.id.widget_title_bar, pendingIntent);
+//
+//
+//        // Set up the collection
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+//            setRemoteAdapter(context, views);
+//        } else {
+//            setRemoteAdapterV11(context, views);
+//        }
+//
+//        // Instruct the widget manager to update the widget
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
+//
+//    }
